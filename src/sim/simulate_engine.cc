bf87caccf8f59e81bd650ceaf7d88a324854fe27
hurricane1026 <hurricane1026@gmail.com>
2026-06-13 17:28:27 +0800

    format simulator includes

--- src/sim/simulate_engine.cc
+++ src/sim/simulate_engine.cc
@@ -11,8 +11,8 @@
 #include "rut/runtime/connection.h"
 #include "rut/runtime/connection_base.h"
 #include "rut/runtime/http_parser.h"
-#include "rut/runtime/sim_engine.h"
 #include "rut/runtime/route_method.h"
+#include "rut/runtime/sim_engine.h"
 #include "rut/runtime/traffic_capture.h"
 #include "rut/runtime/traffic_replay.h"
 
