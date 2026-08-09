03b1e9fbb5472733f6595ba7fe44684cf6eb0cf9
hurricane1026 <hurricane1026@gmail.com>
2026-08-10 00:30:09 +0800

    test: cover route snapshot capture rejection

--- tests/test_control_plane_mutation.cc
+++ tests/test_control_plane_mutation.cc
@@ -2532,6 +2532,14 @@ TEST(control_plane_mutation, signal_snapshot_capture_failures_are_terminalized) 
     }
 }
 
+TEST(control_plane_mutation, route_snapshot_capture_failure_is_rejected_without_record) {
+    ControlPlaneMutationPort port;
+    port.reset(1, true);
+    port.set_reload_source_version_capture(failed_source_capture, nullptr);
+    CHECK_FALSE(port.request_reload(ReloadRequestSource::Route));
+    CHECK_FALSE(port.last_record().valid);
+}
+
 TEST(control_plane_mutation, source_version_capture_only_clears_for_its_registration) {
     ControlPlaneMutationPort port;
     port.reset(1, true);
