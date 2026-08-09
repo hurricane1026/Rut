0111a62a9bc9141ac731706d79b6c5ff1f11e4f8
hurricane1026 <hurricane1026@gmail.com>
2026-08-09 17:27:45 +0800

    fix: restore safe snapshot cache fast path

--- src/reload_coordinator.cc
+++ src/reload_coordinator.cc
@@ -116,6 +116,17 @@ bool ProcessReloadCoordinator::refresh_source_snapshot() {
         route_snapshot_armed_.store(false, std::memory_order_release);
         return false;
     }
+    bool cache_hit = false;
+    {
+        std::lock_guard lock(source_snapshot_mutex_);
+        cache_hit = cached_provider_version_.size() == version_len &&
+                    __builtin_memcmp(cached_provider_version_.data(), version, version_len) == 0;
+    }
+    if (cache_hit) {
+        route_snapshot_armed_.store(true, std::memory_order_release);
+        reclaim_retired_snapshots();
+        return true;
+    }
     char snapshot_source[ReloadRequest::kMaxSourceVersion]{};
     char snapshot_root[ReloadRequest::kMaxSourceVersion]{};
     u32 source_len = 0;
